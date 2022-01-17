1cfb4f331e8b6222cf578f0488276d630f7a0102
Felicity Brammer <felicitybrammer@gmail.com>
2022-01-16 17:47:43 -0700

    header mapping function, logs onclick

--- react-portfolio/src/components/Header/index.js
+++ react-portfolio/src/components/Header/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 function Header() {
-
+    //these links don't go anywhere yet
+    
   const navElements = [
     { name: 'About Me', href: '#about'},
     { name: 'Portfolio', href: '#portfolio'},
     { name: 'Resume', href: '#resume'}
   ];
 
+  function navElSelected(name) {
+      console.log(`${name} clicked`) //works!
+  }
+
   return (
     <header>
         <h2>
             <a href='/'>
-                <span role='img' aria-label='code'><i class="fas fa-code fa-2x"></i></span>
+                <span role='img' aria-label='code'><i className="fas fa-code fa-2x"></i></span>
             </a>
         </h2>
         <nav>
@@ -22,9 +27,11 @@ function Header() {
                         className='mx-2'
                         key={navElement.name}
                         >
-                            <a href={navElement.href}>
-                                <span>{navElement.name}</span>
-                            </a>
+                            <span onClick={navElSelected(navElement.name)} >
+                                <a href={navElement.href}>
+                                    <span>{navElement.name}</span>
+                                </a>
+                            </span>
                         </li>
                 ))}
                 {/* <li className='mx-2'>
